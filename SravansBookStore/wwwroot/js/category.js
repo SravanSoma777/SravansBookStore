258a8e9240fba7a567791cb7e5087fcc291d9fe0
SravanSoma777 <W0766138@myscc.ca>
2022-11-08 15:29:59 -0500

    updated Category.js

--- SravansBookStore/wwwroot/js/category.js
+++ SravansBookStore/wwwroot/js/category.js
@@ -30,11 +30,11 @@ $(document).ready(function () {
     function Delete(url) {
         swal({
             title: "Are you sure you want to delete?",
-            text: "You will not be able to retore data!",
-            icon: "Warning",
+            text: "You will not be able to restore the data!",
+            icon: "warning",
             buttons: true,
             dangerMode: true
-        }).then(willDelete); {
+        }).then((willDelete) => {
             if (willDelete) {
                 $.ajax({
                     type: "DELETE",
@@ -43,12 +43,13 @@ $(document).ready(function () {
                         if (data.success) {
                             toastr.success(data.message);
                             dataTable.ajax.reload();
-                        } else {
+                        }
+                        else {
                             toastr.error(data.message);
                         }
                     }
                 });
             }
-        }
+        });
     }
 });
