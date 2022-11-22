49918b20abe07c300384504dd3a42802b23a8790
SravanSoma777 <W0766138@myscc.ca>
2022-11-21 21:35:33 -0500

    Product Index View Created

--- SravansBooks.DataAccess/Repository/IRepository/IProductRepository.cs
+++ SravansBooks.DataAccess/Repository/IRepository/IProductRepository.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace SravansBooks.DataAccess.Repository.IRepository
 {
-    public interface IProductRepository
+    public interface IProductRepository : IRepository<Product>
     {
         void Update(Product product);
     }
