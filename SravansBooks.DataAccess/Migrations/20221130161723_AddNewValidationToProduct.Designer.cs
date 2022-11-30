3d155d220f62633abf5681834477922730c5e311
SravanSoma777 <W0766138@myscc.ca>
2022-11-30 14:24:28 -0500

    Added migration to new properties and validations

--- SravansBooks.DataAccess/Migrations/20221130161723_AddNewValidationToProduct.Designer.cs
+++ SravansBooks.DataAccess/Migrations/20221130161723_AddNewValidationToProduct.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SravansBookStore.DataAccess.Data;
 
 namespace SravansBooks.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221130161723_AddNewValidationToProduct")]
+    partial class AddNewValidationToProduct
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
